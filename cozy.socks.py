a3066140b2cb7830c721487129e6da2561a18780
bogdan-mescheryakov <78803221+bogdan-mescheryakov@users.noreply.github.com>
2021-03-08 16:51:58 +0800

    Add files via upload
    example

---
+++ cozy.socks.py
@@ -0,0 +1,56 @@
+cozy = 'cozy.socks'
+print(cozy)
+
+a_pair =  1
+a_set = 5*a_pair
+first_purchase = (a_set)*80
+price = a_pair*13
+fp_price = first_purchase*price
+
+kg = 281
+fp_delivery = 20*kg
+delivery_1pai = fp_delivery/400
+print("one pair delivery price:" + str(delivery_1pai))
+one_pair_sebestoimost = delivery_1pai + price
+print("one pair sebestoimost" + str(one_pair_sebestoimost))
+
+goods = fp_price + fp_delivery
+advertisment = 557 + 1520
+expenses = goods + advertisment
+print('Our expenses for now are: ' + str(expenses))
+
+pair_sold = 7
+sell_price = 89
+earnings = sell_price*pair_sold
+max_earning = sell_price*first_purchase
+max_profit = max_earning - expenses - earnings
+print('Maximum possible profit: ' + str(max_profit))
+print('We have already earned: ' + str(earnings))
+
+
+
+m_heel = ['m_h red', 'm_h yellow', 'm_h orange', 'm_h khaki', 'm_h blue']
+w_heel = ['h brown', 'h pink(dark)', 'h pink(light)', 'h beige', 'h grey']
+heart = ['h white', 'h blue', 'h pink', 'h black', 'h grey']
+w_2lines = []
+m_2lines = []
+zebra = []
+_5_lines = []
+_18 = []
+w_plain = []
+m_plain = []
+sand = []
+
+print(goods)
+
+
+
+
+
+socks_types = []
+
+
+
+
+
+
